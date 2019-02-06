e07c38606fdd01b66443643a0845f9263af6d9e2
Michelle Musumba <andenyimichelle@gmail.com>
2019-02-07 00:41:37 +0300

    [Starts #163729099]view all political offices

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask,jsonify,make_response
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+office_list=[]
+
+
+@app.route('/offices',methods=['GET'])
+def offices():
+    return make_response(jsonify({
+        "offices":office_list,
+        "status":"200"
+    }), 200)
+
+
+if __name__=='__main__':
+    app.run()
