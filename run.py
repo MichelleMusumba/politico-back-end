e722a06dd3e5a9fbf053a39a29788d1830fec74d
Michelle Musumba <andenyimichelle@gmail.com>
2019-02-07 23:09:52 +0300

    [starts ft-create-a-political-party-163742854]creates a political party

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
+
 app = Flask(__name__)
 
 office_list = []
+party_list = []
 
 
-@app.route('/offices', methods = ['GET'])
+@app.route('/offices', methods=['GET'])
 def offices():
     return make_response(jsonify({
         "offices": office_list,
@@ -12,25 +14,22 @@ def offices():
     })), 200
 
 
-party_list = []
+@app.route('/parties', methods=['POST'])
+def party():
+    data = request.get_json()
+    party_name = data['name']
+    party_address = data['hqAddress']
+    party_logo = data['logoUrl']
 
+    new_party = {
+        "id": len(party_list) + 1,
+        "name": party_name,
+        "hqAddress": party_address,
+        "logoUrl": party_logo
+    }
+    party_list.append(new_party)
 
-@app.route('/parties', methods=['GET'])
-def parties():
-    return make_response(jsonify({
-        "parties": party_list,
-        "status": "200"
-    }), 200)
-
-
-
-@app.route('/parties', methods=['GET'])
-def parties():
-    return make_response(jsonify({
-        "parties": party_list,
-        "status": "200"
-    }), 200)
-
+    return make_response(jsonify({"status": 201, "data": [new_party]}), 201)
 
 
 if __name__ == '__main__':
